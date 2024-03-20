8d540a66242f8419c69609a8c3509a5a100e4366
omercakir2 <omercakir2323@gmail.com>
2024-03-20 09:11:55 +0300

    add some changes , still not working though

--- stock2.py
+++ stock2.py
@@ -3,18 +3,37 @@ class Solution(object):
             maxprof = 0
             lenght = len(prices)
             if all(prices[i] >= prices[i+1] for i in range(lenght-1)):
-                maxprof = 0 
+                maxprof = 0  # making profit is not possible in this case
                 print(maxprof)
             elif all(prices[i] <= prices[i+1] for i in range(lenght-1)):
                 maxprof = prices[lenght-1] - prices[0]
                 print(maxprof)
 
             else :
-                print(maxprof)
+                sell = 0
+                buy = 0
+                profit = 0
+                i = 0
+                
+                while i < lenght-1:
+                    if prices[i+1] > prices[i]:
+                        buy = prices[i]
+                        for j in range(i,lenght-1,1):
+                            if prices[j+1] < prices[j]:
+                                sell = prices[j]
+                                i = j
+                        profit = profit + sell - buy 
+                        #print(f"buy  = {buy} , sell = {sell} , profit = {profit} , i = {i}")
+                    else : 
+                        i +=1
+                    print(f"buy  = {buy} , sell = {sell} , profit = {profit} , i = {i-1} ")
+                        
+                        
 
-list1 = [1,2,3]
-list2 = [3,2,1]
-list3 = [1,3,2]
+                     
+                #print(f"max profit {profit}")
+
+       
+
+list1 = [7,1,5,3,6,4]
 Solution.maxProfit(list1)
-Solution.maxProfit(list2)
-Solution.maxProfit(list3)
