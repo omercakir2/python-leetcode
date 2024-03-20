6140a9e292bbd5dcc6c80babbf5a8485ffd3ff9e
omercakir2 <omercakir2323@gmail.com>
2024-03-20 18:41:14 +0300

    fix the code completely , leetcode accepted it

--- stock2.py
+++ stock2.py
@@ -1,6 +1,7 @@
 class Solution(object):
         def maxProfit(prices):
             lenght = len(prices)
+            maxprof = 0
             if all(prices[i] >= prices[i+1] for i in range(lenght-1)):
                 maxprof = 0  # making profit is not possible in this case
                 print(maxprof)
@@ -11,29 +12,27 @@ class Solution(object):
             else :
                 sell = 0
                 buy = 0
-                profit = 0
                 i = 0
                 
                 while i < lenght-1:
                     if prices[i+1] > prices[i]:
                         buy = prices[i]
+                        if all(prices[k+1]>=prices[k] for k in range(i,lenght-1,1)):
+                             sell = prices[lenght-1]
+                             maxprof += sell - buy 
+                             break
                         for j in range(i,lenght-1,1):
                             if prices[j+1] < prices[j]:
-                                sell = prices[j]
-                                i = j
-                                break
-                        profit = profit + sell - buy 
-                        #print(f"buy  = {buy} , sell = {sell} , profit = {profit} , i = {i}")
-                    else : 
-                        i +=1
-                    print(f"buy  = {buy} , sell = {sell} , profit = {profit} , i = {i-1} ")
-                        
-                        
-
+                                 sell = prices[j]
+                                 i = j
+                                 maxprof += sell - buy
+                                 break
+                    i += 1
+            print(maxprof) 
+                            
                      
-                #print(f"max profit {profit}")
+                 
+list1 = [1,9,6,9,1,7,1,1,5,9,9,9]
 
-       
 
-list1 = [7,1,5,3,6,4]
 Solution.maxProfit(list1)
