6544964c3e3cb2ea3e2026e73404baeb1937d699
omercakir2 <omercakir2323@gmail.com>
2024-03-24 12:59:29 +0300

    write by using a different approach

---
+++ jump-without-rec.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def canJumpornot(self,arr):
+        last_indx = len(arr)-1
+        for i in range(len(arr)-1,-1,-1): # we're doing it starting from ending to beginning 
+            if i + arr[i] >= last_indx: 
+                last_indx = i #every time we found a spot which is proper to jump, we update the last_indx
+        
+        boolen = last_indx == 0
+        print(boolen)
+        return(boolen)
+
+list1 = [2,0,6,9,8,4,5,0,8,9,1,2,9,6,8,8,0,6,3,1,2,2,1,2,6,5,3,1,2,2,6,4,2,4,3,0,0,0,3,8,2,4,0,1,2,0,1,4,6,5,8,0,7,9,3,4,6,6,5,8,9,3,4,3,7,0,4,9,0,9,8,4,3,0,7,7,1,9,1,9,4,9,0,1,9,5,7,7,1,5,8,2,8,2,6,8,2,2,7,5,1,7,9,6]
+Solution.canJumpornot(Solution,list1)
