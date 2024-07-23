125dc3d6cba257ff3566ee1cc8becf899218ba86
omercakir2 <omercakir2323@gmail.com>
2024-07-23 16:15:30 +0300

    merge 2 linked list

---
+++ linkedlist/merge2lnkd.py
@@ -0,0 +1,37 @@
+#In this code , my purpose is to merge 2 linked integer list which are ordered by decreasing order
+#Also to learn how to manage linked lists.
+
+# Definition for singly-linked list.
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        num1 = list1.val
+        num2 = list2.val
+
+
+ll = LinkedList()
+while 
+
+        
